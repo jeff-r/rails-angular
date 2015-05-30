ea34f773fc127bb9a2af26e2ba546bca2fbebbcd
Jeff Roush <jeff@jeffroush.com>
2015-05-30 14:31:48 -0700

    Skip verify_authenticity_token

--- rails/app/controllers/projects_controller.rb
+++ rails/app/controllers/projects_controller.rb
@@ -1,4 +1,5 @@
 class ProjectsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
   before_action :set_project, only: [:show, :edit, :update, :destroy]
 
   # GET /projects
